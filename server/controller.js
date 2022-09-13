8a4ddfd323d9cee4958505417244bc0f3db8b201
Matthew Rawlings <mbrawlings11@gmail.com>
2022-09-13 11:16:19 -0600

    heroku & pgweb setup

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,16 @@
 const path = require('path')
+require('dotenv').config()
+const {CONNECTION_STRING} = process.env
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
 
 let favClimbs = []
 let globalId = 1
